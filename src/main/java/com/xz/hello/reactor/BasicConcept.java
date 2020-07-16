e85596100ecb8bc4302c251d080afdf68c23ef22
zjw <1300151235@qq.com>
2020-07-16 16:42:03 +0800

    添加一些test和note

--- src/main/java/com/xz/hello/reactor/BasicConcept.java
+++ src/main/java/com/xz/hello/reactor/BasicConcept.java
@@ -4,8 +4,12 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 public class BasicConcept {
@@ -19,6 +23,10 @@ public class BasicConcept {
          *
          * 完成信号用于告知下游订阅者该数据流正常结束，错误信号终止数据流的同时将错误传递给下游订阅者。
          */
+/*
+        testJust();
+        testList();
+        testMon();*/
 
 
         /**
@@ -103,8 +111,109 @@ public class BasicConcept {
     private static Flux<Integer> generateFluxFrom1To6() {
         return Flux.just(1, 2, 3, 4, 5, 6);
     }
+
     private static Mono<Integer> generateMonoWithError() {
         return Mono.error(new Exception("some error"));
+    }
+
+
+    public static void testJust() {
+        Flux.just("hello", "world")
+                .subscribe(System.out::println);
+    }
+
+    public static void testList() {
+        List<String> words = Arrays.asList(
+                "hello",
+                "reactive",
+                "world"
+        );
+
+        Flux.fromIterable(words)
+                .subscribe(System.out::println);
+    }
+
+    public static void testRange() {
+        Flux.range(1, 10)
+                .subscribe(System.out::println);
+    }
+
+    public static void testInterval() {
+        Flux.interval(Duration.ofSeconds(1))
+                .subscribe(System.out::println);
+    }
+
+    public static void testMon() {
+        displayCurrTime(1);
+        displayCurrThreadId(1);
+        //创建一个数据源
+        Mono.just(10)
+                //延迟5秒再发射数据
+                .delayElement(Duration.ofSeconds(5))
+                //在数据上执行一个转换
+                .map(n -> {
+                    displayCurrTime(2);
+                    displayCurrThreadId(2);
+                    displayValue(n);
+                    delaySeconds(2);
+                    return n + 1;
+                })
+                //在数据上执行一个过滤
+                .filter(n -> {
+                    displayCurrTime(3);
+                    displayCurrThreadId(3);
+                    displayValue(n);
+                    delaySeconds(3);
+                    return n % 2 == 0;
+                })
+                //如果数据没了就用默认值
+                .defaultIfEmpty(9)
+                //订阅一个消费者把数据消费了
+                .subscribe(n -> {
+                    displayCurrTime(4);
+                    displayCurrThreadId(4);
+                    displayValue(n);
+                    delaySeconds(2);
+                    System.out.println(n + " consumed, worker Thread over, exit.");
+                });
+        displayCurrTime(5);
+        displayCurrThreadId(5);
+        pause();
+    }
+
+    //显示当前时间
+    static void displayCurrTime(int point) {
+        System.out.println(point + " : " + LocalTime.now());
+    }
+
+    //显示当前线程Id
+    static void displayCurrThreadId(int point) {
+        System.out.println(point + " ThreadId: " + Thread.currentThread().getId());
+    }
+
+    //显示当前的数值
+    static void displayValue(int n) {
+        System.out.println("input : " + n);
+    }
+
+    //延迟若干秒
+    static void delaySeconds(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //主线程暂停
+    static void pause() {
+        try {
+            System.out.println("main Thread over, paused.");
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
--- src/test/java/com/xz/ReactorTest.java
+++ src/test/java/com/xz/ReactorTest.java
@@ -5,14 +5,23 @@ import org.junit.Test;
 import org.reactivestreams.Subscription;
 import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
+import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+/**
+ * 详见   https://htmlpreview.github.io/?https://github.com/get-set/reactor-core/blob/master-zh/src/docs/index.html#about-doc
+ */
 
 public class ReactorTest {
 
@@ -221,4 +230,269 @@ public class ReactorTest {
     }
 
 
+    @Test
+    public void test() {
+
+    }
+
+    /**
+     * 可编程式地创建一个序列Generate
+     */
+
+
+    /**
+     * 基于状态值的 generate 示例
+     * 初始化状态值（state）为0。
+     * 我们基于状态值 state 来生成下一个值（state 乘以 3）。
+     * 我们也可以用状态值来决定什么时候终止序列。
+     * 返回一个新的状态值 state，用于下一次调用。
+     */
+
+    @Test
+    public void testGenerate() {
+        Flux<String> flux = Flux.generate(
+                () -> 0,
+                (state, sink) -> {
+                    sink.next("3 x " + state + " = " + 3 * state);
+                    if (state == 10) sink.complete();
+                    return state + 1;
+                });
+        flux.subscribe(System.out::println);
+    }
+
+    /**
+     * 可变类型的状态变量
+     */
+    @Test
+    public void testGenerate1() {
+        Flux<String> flux = Flux.generate(
+                AtomicLong::new,
+                (state, sink) -> {
+                    long i = state.getAndIncrement();
+                    sink.next("3 x " + i + " = " + 3 * i);
+                    if (i == 9) sink.complete();
+                    return state;
+                });
+        flux.subscribe(System.out::println);
+    }
+
+    @Test
+    public void testGenerate2() {
+        Flux<String> flux = Flux.generate(
+                AtomicLong::new,
+                (state, sink) -> {
+                    long i = state.getAndIncrement();
+                    sink.next("3 x " + i + " = " + 3 * i);
+                    if (i == 10) sink.complete();
+                    return state;
+                }, (state) -> System.out.println("state: " + state));
+        flux.subscribe(System.out::println);
+    }
+
+    /**
+     * 可编程式地创建一个序列Create
+     * <p>
+     * create 有个好处就是可以将现有的 API 转为响应式，比如监听器的异步方法。
+     */
+
+    @Test
+    public void testCreate() {
+        Flux.create(sink -> {
+            for (int i = 0; i < 10; i++) {
+                sink.next(i);
+            }
+            sink.complete();
+        }).subscribe(System.out::println);
+    }
+
+
+    interface MyEventListener<T> {
+        void onDataChunk(List<T> chunk);
+
+        void processComplete();
+    }
+
+
+    /**
+     * 官网的Create和push暂时不知道myEventProcessor是什么
+     */
+    @Test
+    public void testCreate1() {
+/*        Flux<String> bridge = Flux.create(sink -> {
+            myEventProcessor.register(
+                    new MyEventListener<String>() {
+
+                        public void onDataChunk(List<String> chunk) {
+                            for (String s : chunk) {
+                                sink.next(s);
+                            }
+                        }
+
+                        public void processComplete() {
+                            sink.complete();
+                        }
+                    });
+        });*/
+    }
+
+    /**
+     * Handle
+     * <p>
+     * handle 方法有些不同，它在 Mono 和 Flux 中都有。然而，它是一个实例方法 （instance method），意思就是它要链接在一个现有的源后使用（与其他操作符一样
+     */
+    @Test
+    public void testHandle() {
+        Flux<String> alphabet = Flux.just(-1, 30, 13, 9, 20)
+                .handle((i, sink) -> {
+                    String letter = alphabet(i);
+                    if (letter != null)
+                        sink.next(letter);
+                });
+
+        alphabet.subscribe(System.out::println);
+    }
+
+    public String alphabet(int letterNumber) {
+        if (letterNumber < 1 || letterNumber > 26) {
+            return null;
+        }
+        int letterIndexAscii = 'A' + letterNumber - 1;
+        return "" + (char) letterIndexAscii;
+    }
+
+
+    /**
+     * 调度器（Schedulers）
+     * <p>
+     * Scheduler 是一个拥有广泛实现类的抽象接口。 Schedulers 类提供的静态方法用于达成如下的执行环境
+     * <p>
+     * 当前线程（Schedulers.immediate()）
+     * <p>
+     * 可重用的单线程（Schedulers.single()) 注意，这个方法对所有调用者都提供同一个线程来使用， 直到该调度器（Scheduler）被废弃。如果你想使用专一的线程，就对每一个调用使用 Schedulers.newSingle()
+     * <p>
+     * 弹性线程池（Schedulers.elastic()   它根据需要创建一个线程池，重用空闲线程。线程池如果空闲时间过长 （默认为 60s）就会被废弃。对于 I/O 阻塞的场景比较适用。 Schedulers.elastic() 能够方便地给一个阻塞 的任务分配它自己的线程，从而不会妨碍其他任务和资源
+     * <p>
+     * 固定大小线程池（Schedulers.parallel())   所创建线程池的大小与 CPU 个数等同
+     */
+
+    @Test
+    public void testSchedulers1() throws InterruptedException, IOException {
+        System.out.println("main thread:" + Thread.currentThread().getId());
+        CountDownLatch countDownLatch = new CountDownLatch(1);  // 2
+        Flux.interval(Duration.ofMillis(300), Schedulers.newSingle("test")).subscribe((n -> {
+            System.out.println(n);
+            System.out.println(Thread.currentThread().getId() + "----" + Thread.currentThread().getName());
+        }), null, countDownLatch::countDown);
+
+        countDownLatch.await(10, TimeUnit.SECONDS);     // 5
+
+/*        Flux.interval(Duration.ofMillis(300), Schedulers.newSingle("test")).limitRequest(10).subscribe(System.out::println);;
+        System.in.read();*/
+    }
+
+    /**
+     * Reactor 提供了两种在响应式链中调整调度器 Scheduler 的方法：publishOn 和 subscribeOn。
+     *
+     * 它们都接受一个 Scheduler 作为参数，从而可以改变调度器。但是 publishOn 在链中出现的位置 是有讲究的，而 subscribeOn 则无所谓。
+     *
+     * 在 Reactor 中，当你在操作链上添加操作符的时候，你可以根据需要在 Flux 和 Mono 的实现中包装其他的 Flux 和 Mono。一旦你订阅（subscribe）了它
+     *
+     * 一个 Subscriber 的链 就被创建了，一直向上到第一个 publisher 。这些对开发者是不可见的，开发者所能看到的是最外一层的 Flux （或 Mono）和 Subscription
+     *
+     * 但是具体的任务是在中间这些跟操作符相关的 subscriber 上处理的
+     *
+     * publishOn 的用法和处于订阅链（subscriber chain）中的其他操作符一样。它将上游 信号传给下游，同时执行指定的调度器 Scheduler 的某个工作线程上的回调。
+     *
+     * 它会 改变后续的操作符的执行所在线程 （直到下一个 publishOn 出现在这个链上）
+     *
+     * subscribeOn 用于订阅（subscription）过程，作用于那个向上的订阅链（发布者在被订阅 时才激活，订阅的传递方向是向上游的）。
+     *
+     * 所以，无论你把 subscribeOn 至于操作链的什么位置， 它都会影响到源头的线程执行环境（context）。 但是，它不会影响到后续的 publishOn，后者仍能够切换其后操作符的线程执行环境
+     *
+     * 只有操作链中最早的 subscribeOn 调用才算数。
+     *
+     * subscribe() 之前什么都不会发生
+     *
+     * 在 Reactor 中，当你创建了一条 Publisher 处理链，数据还不会开始生成。事实上，你是创建了 一种抽象的对于异步处理流程的描述（从而方便重用和组装）。
+     *
+     * 当真正“订阅（subscrib）”的时候，你需要将 Publisher 关联到一个 Subscriber 上，然后 才会触发整个链的流动。这时候，Subscriber 会向上游发送一个 request 信号，一直到达源头 的 Publisher
+     *
+     */
+
+
+    /**
+     * 调试
+     */
+
+
+    /**
+     * Hooks.onOperatorDebug();
+     * <p>
+     * 用这种形式的检测方式构造 stack trace 是成本较高的。也因此这种调试模式作为最终大招， 只应该在可控的方式下激活。
+     */
+    @Test
+    public void testBug() {
+        Hooks.onOperatorDebug();
+
+        Flux.range(1, 6)
+                .map(i -> 10 / (i - 3))
+                .map(i -> i * i)
+                .subscribe();
+    }
+
+
+    /**
+     * 用 checkpoint() 方式替代
+     * <p>
+     * 如果你能确定是在你的代码中组装的响应式链存在问题，而且程序的可服务性又是很重要的， 那么你可以 使用 checkpoint() 操作符。
+     * <p>
+     * 它有两种调试技术可用
+     * <p>
+     * 1--你可以把这个操作符加到链中。这时 checkpoint 操作符就像是一个 hook，但只对它所在的链起作用。
+     * <p>
+     * 2--还有一个 checkpoint(String) 的方法变体，你可以传入一个独特的字符串以方便在 assembly traceback 中识别信息。
+     * <p>
+     * 这样会省略 stack trace，你可以依赖这个字符串（以下改称“定位描述符”）来定位到组装点。checkpoint(String) 比 checkpoint 有更低的执行成本。
+     * <p>
+     * 最后的但同样重要的是，如果你既想通过 checkpoint 添加定位描述符，同时又依赖于 stack trace 来定位组装点，你可以使用 checkpoint("description", true)
+     * <p>
+     * 来实现这一点。这时回溯信息又出来了， 同时附加了定位描述符，如下例所示
+     */
+    @Test
+    public void testBug1() {
+        Flux.range(1, 6)
+                .map(i -> 10 / (i - 3))
+                .map(i -> i * i)
+                .checkpoint()
+                .subscribe();
+    }
+
+    @Test
+    public void testBug2() {
+        Flux.range(1, 6)
+                .map(i -> 10 / (i - 3))
+                .map(i -> i * i)
+                .checkpoint("bug~~~~~~")
+                .subscribe();
+    }
+
+    @Test
+    public void testBug3() {
+        Flux.range(1, 6)
+                .map(i -> 10 / (i - 3))
+                .map(i -> i * i)
+                .checkpoint("bug~~~~~~", true)
+                .subscribe();
+    }
+
+    /**
+     * log
+     */
+    @Test
+    public void testLog() {
+        Flux.range(1, 10)
+                .log()
+                .take(3).subscribe(System.out::println);
+    }
 }
+
